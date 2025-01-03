06680cb6d65a336941327e1387a88e750ba86a9b
Salim66 <salimhasanriad@gmail.com>
2025-01-03 21:41:03 +0600

    add user contorller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,9 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller('api')
+export class UserController {
+    @Post("/register")
+    register(@Body() body: any) {
+        return body;
+    }
+}
