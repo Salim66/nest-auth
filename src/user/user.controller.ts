feac268d913306bf8b19038cfbf5306cdbfa4792
Salim66 <salimhasanriad@gmail.com>
2025-01-03 21:57:30 +0600

    Add: create service and user registration successfully

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import * as bcryptjs from 'bcryptjs';
 
 @Controller('api')
 export class UserController {
+    constructor(private userService: UserService) { }
+    
     @Post("/register")
-    register(@Body() body: any) {
-        return body;
+    async register(@Body() body: any) {
+        if (body.password !== body.password_confirm) {
+            throw new BadRequestException("Password do not match!");
+        }
+
+        return this.userService.save({
+            first_name: body?.first_name,
+            last_name: body.last_name,
+            email: body?.email,
+            password: await bcryptjs.hash(body?.password, 12)
+        })
     }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(User) protected readonly userRepository: Repository<User>
+    ) { }
+    
+    async save(body) {
+        return await this.userRepository.save(body);
+    }
+}
